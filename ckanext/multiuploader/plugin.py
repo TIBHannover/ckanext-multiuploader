eedede66e0ab294342b50fca8e2f1a7f6110b7be
pooya.oa <pooya.oladazimi@tib.eu>
2021-05-12 17:03:37 +0200

    added package templates and a custom snippet to overwrite resource upload default template

--- ckanext/multiuploader/plugin.py
+++ ckanext/multiuploader/plugin.py
@@ -11,6 +11,7 @@ class MultiuploaderPlugin(plugins.SingletonPlugin):
     # IConfigurer
 
     def update_config(self, config_):
+        toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'public')
         toolkit.add_resource('fanstatic',
             'multiuploader')
