56c2188152ea50227958f9bcad7d1a0128ff0507
pooya.oa <pooya.oladazimi@tib.eu>
2021-05-17 11:17:44 +0200

    changed resource uploading form to act on Ajax + implemented file names box

--- ckanext/multiuploader/public/JS/multi_uploader.js
+++ ckanext/multiuploader/public/JS/multi_uploader.js
@@ -1,6 +1,7 @@
+var fileList = [];
 $(document).ready(function(){
     $('#UpBtn').on('click', function() {    
-        if ($('#UpBtn').hasClass('uploaded')){
+        if ($('#UpBtn').hasClass('uploaded')){ // Remove all files
             $('#LinkBtn').show();
             $('#UpBtn').css('background-color', 'white');
             $('#UpBtn').css('color', 'black');
@@ -8,6 +9,8 @@ $(document).ready(function(){
             $('#UpBtn').text('Upload');
             $('#UpBtn').removeClass('uploaded');
             $('#fileUpload').val('');
+            fileList = [];
+            $('#fileNames').empty();            
         }
         else{
             $('#fileUpload').trigger('click');
@@ -18,7 +21,17 @@ $(document).ready(function(){
         var files = $("#fileUpload")[0].files;
         for (var i = 0; i < files.length; i++)
         {
-            console.info(files[i].name);
+            fileList.push(files[i]);            
+        }        
+        var filesBox = $('#fileNames');
+        let elem = "<div class='fileItem' id='ID'>FILE  <i class='fa fa-close'></i></div>";
+        for (var i = 0; i < fileList.length; i++)
+        {
+            elem = elem.replace('ID', i);
+            elem = elem.replace('FILE', fileList[i].name);
+            filesBox.append(elem);
+            elem = "<div class='fileItem' id='ID'>FILE  <i class='fa fa-close'></i></div>";
+
         }
         $('#LinkBtn').hide();
         $('#UpBtn').css('background-color', 'red');
@@ -40,5 +53,41 @@ $(document).ready(function(){
         $('#urlBox').hide();        
     });
 
+    $(document).on('click', '.fileItem', function(e){
+        if($(e.target).is('i')){
+            let idx = parseInt($(this).attr('id')); 
+            fileList.splice(idx, 1);
+            $(this).remove();
+
+        }
+        if($('.fileItem').length === 0){
+            $('#UpBtn').click();
+        }
+
+    });
+
+    $("#resource-edit").bind('submit', function (e) {
+        var isValid = formValidity();
+        if (isValid) {
+            jQuery.ajax({
+                type: "POST",
+                url: "/multiuploader/upload_resources",
+                dataType: "html",
+                data: {'description': $('#field-description').val(), 'files':fileList},
+                success: function (result) {
+                    console.log(result);
+                }
+            });
+        }
+        e.preventDefault();
+        return false;
+    });
+
 });
 
+function formValidity(){
+    if($("#fileUpload")[0].files){
+        return true;
+    }
+    return false
+}
