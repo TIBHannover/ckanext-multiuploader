7a2959dce75753c641169f59f9d5461beefcb787
pooya.oa <pooya.oladazimi@tib.eu>
2021-05-21 13:39:52 +0200

    added form validation for the controller

--- ckanext/multiuploader/controllers.py
+++ ckanext/multiuploader/controllers.py
@@ -7,21 +7,25 @@ import ckan.plugins.toolkit as toolkit
 
 class UploadController():
 
-    def upload_resources():
-        base_url = h.get_site_protocol_and_host()[0] + '://' + h.get_site_protocol_and_host()[1]
-        if toolkit.g.user:                
-            package_name = request.form['pck_id']
-            action = request.form['save']
-            if action == "go-dataset":  # Previous button: go back to the dataset metadat                 
-                return h.url_for('dataset.edit', id=str(package_name) ,  _external=True)
+    def upload_resources():        
+        if toolkit.g.user: 
+            try:               
+                package_name = request.form['pck_id']
+                action = request.form['save']
+                if action == "go-dataset":  # Previous button: go back to the dataset metadat                 
+                    return h.url_for('dataset.edit', id=str(package_name) ,  _external=True)
 
-            elif action == "go-dataset-complete": # Add resource to an active dataset
-                Helper.add_resource(package_name, request, False, int(request.form['isLink']))                
-                return h.url_for('dataset.read', id=str(package_name) ,  _external=True)
-            
-            else: # Add resource to a draft dataset
-                Helper.add_resource(package_name, request, True, int(request.form['isLink']))                            
-                return h.url_for('dataset.read', id=str(package_name) ,  _external=True)
+                elif action == "go-dataset-complete": # Add resource to an active dataset
+                    Helper.add_resource(package_name, request, False, int(request.form['isLink']))                
+                    return h.url_for('dataset.read', id=str(package_name) ,  _external=True)
+                
+                else: # Add resource to a draft dataset
+                    Helper.add_resource(package_name, request, True, int(request.form['isLink']))                            
+                    return h.url_for('dataset.read', id=str(package_name) ,  _external=True)
+            except:
+                return toolkit.abort(400, "missing data")
+
+
         else:
             return toolkit.abort(403, "You need to authenticate before accessing this function" )
             
