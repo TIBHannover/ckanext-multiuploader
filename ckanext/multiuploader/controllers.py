613f9c3ee5b2aebb257feccfab7169731c407be2
pooya.oa <pooya.oladazimi@tib.eu>
2021-06-17 15:19:20 +0200

    make the redirection compatible with group_ownership plugin

--- ckanext/multiuploader/controllers.py
+++ ckanext/multiuploader/controllers.py
@@ -21,7 +21,10 @@ class UploadController():
                 
                 else: # Add resource to a draft dataset
                     Helper.add_resource(package_name, request, True, int(request.form['isLink']))
-                    if Helper.check_plugin_enabled("media_wiki"): # if media_wiki plugin exists
+                    if Helper.check_plugin_enabled("group_ownership"): # if group_ownership plugin exists:
+                        return h.url_for('group_ownership.add_ownership_view', id=str(package_name) ,  _external=True)
+
+                    elif Helper.check_plugin_enabled("media_wiki"): # if media_wiki plugin exists
                         return h.url_for('media_wiki.machines_view', id=str(package_name) ,  _external=True)
 
                     return h.url_for('dataset.read', id=str(package_name) ,  _external=True)
