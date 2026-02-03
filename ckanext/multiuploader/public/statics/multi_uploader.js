7873023ab4ff9236bb12a1fe72399a495edc7f53
Bhavin Katabathuni <bhavin2897@gmail.com>
2026-02-03 09:10:36 +0100

    fix: hide close button

--- ckanext/multiuploader/public/statics/multi_uploader.js
+++ ckanext/multiuploader/public/statics/multi_uploader.js
@@ -187,7 +187,7 @@ $(document).ready(function () {
       $('#cancel_waiting').hide();
       $('.modal-title').show();
       $('#upload-cancel').show();
-      $('#upload-progress-modal-close').show();
+      $('#upload-progress-modal-close').hide();
 
       $('#progress-bar-container').show();
       $('#upload-error-container').hide();
